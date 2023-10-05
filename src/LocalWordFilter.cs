013b478de4c8ac98fa67eb32048fd570197d0988
Edi Wang <Edi.Wang@outlook.com>
2023-10-05 17:07:38 +0800

    Update LocalWordFilter.cs

--- src/LocalWordFilter.cs
+++ src/LocalWordFilter.cs
@@ -10,11 +10,21 @@ namespace Moonglade.ContentSecurity;
 public static class LocalWordFilter
 {
     [FunctionName("Mask")]
-    public static async Task<IActionResult> Run(
+    public static async Task<IActionResult> Mask(
         [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] Payload req,
         ILogger log)
     {
         log.LogInformation("C# HTTP trigger function Mask processed a request.");
+
+        return new OkResult();
+    }
+
+    [FunctionName("Detect")]
+    public static async Task<IActionResult> Detect(
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] Payload req,
+        ILogger log)
+    {
+        log.LogInformation("C# HTTP trigger function Detect processed a request.");
 
         return new OkResult();
     }
