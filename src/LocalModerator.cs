664920a4adee69e4c979b51b3fe1585820e69d5d
Edi Wang <Edi.Wang@outlook.com>
2023-10-05 17:10:18 +0800

    Create LocalModerator.cs

---
+++ src/LocalModerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Edi.WordFilter;
+
+namespace Moonglade.ContentSecurity;
+
+public class LocalModerator : ICommentModerator
+{
+    private readonly IMaskWordFilter _filter;
+
+    public LocalModerator(string words)
+    {
+        var sw = new StringWordSource(words);
+        _filter = new MaskWordFilter(sw);
+    }
+
+    public Task<string> ModerateContent(string input) => Task.FromResult(_filter.FilterContent(input));
+
+    public Task<bool> HasBadWord(params string[] input) => Task.FromResult(input.Any(s => _filter.ContainsAnyWord(s)));
+}
