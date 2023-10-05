6f1f2c98d8fe29ae88f0b710af9c1659d742d863
Edi Wang <Edi.Wang@outlook.com>
2023-10-05 17:09:11 +0800

    Create ICommentModerator.cs

---
+++ src/ICommentModerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Moonglade.ContentSecurity;
+
+public interface ICommentModerator
+{
+    public Task<string> ModerateContent(string input);
+
+    public Task<bool> HasBadWord(params string[] input);
+}
