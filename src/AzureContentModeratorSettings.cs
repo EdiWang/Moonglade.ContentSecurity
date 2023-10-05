d2d142c9e34cdc9b662c4092963aa001eac781ad
Edi Wang <Edi.Wang@outlook.com>
2023-10-05 17:11:07 +0800

    Create AzureContentModeratorSettings.cs

---
+++ src/AzureContentModeratorSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Moonglade.ContentSecurity;
+
+public class AzureContentModeratorSettings
+{
+    public string OcpApimSubscriptionKey { get; set; }
+    public string Endpoint { get; set; }
+}
